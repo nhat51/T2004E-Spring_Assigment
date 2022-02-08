bdfb808f56592c83382c6a031b3ad112e02d3743
nhat51 <65001571+nhat51@users.noreply.github.com>
2022-02-08 15:20:47 +0700

    update

--- front/src/pages/street/List/index.vue
+++ front/src/pages/street/List/index.vue
@@ -1,28 +1,28 @@
 <template>
   <div>
     <a-card>
-<!--      <div>
-        <a-form class="product__search-form">
+      <div>
+        <a-form @submit="handleSearch" class="product__search-form">
           <a-form-item>
-            <a-input-search placeholder="Search by name">
+            <a-input-search v-model="param.name" placeholder="Search by name">
               <a-icon type="search"/>
             </a-input-search>
           </a-form-item>
           <a-form-item>
-            <a-select placeholder="Search by District">
-              <a-select-option >
-
+            <a-select v-model="param.districtId" placeholder="Search by District">
+              <a-select-option v-for="item in district" :key="item.id" :value="item.id">
+                {{item.name}}
               </a-select-option>
             </a-select>
           </a-form-item>
         </a-form>
-        <a-button type="primary" html-type="submit" >
+        <a-button type="primary" html-type="submit" @click="handleSearch">
           Search
         </a-button>
-        <a-button type="primary" html-type="submit" style="margin-left: 20px">
+        <a-button type="primary" html-type="submit" style="margin-left: 20px" @click="resetButton">
           Reset
         </a-button>
-      </div>-->
+      </div>
       <div style="padding-bottom: 20px; padding-top: 20px">
         <router-link to="/streets/add">
           <a-button type="primary" icon="plus">
@@ -91,7 +91,11 @@ export default {
   return{
     data: [],
     columns,
-    district: []
+    district: [],
+    param:{
+      name: undefined,
+      districtId:undefined
+    }
   }
   },
 created() {
@@ -100,7 +104,7 @@ created() {
 },
 methods:{
     getStreets(){
-      StreetService.getAll().then(
+      StreetService.getAll(this.param).then(
           res =>{
             console.log(res.data)
             this.data = res.data
@@ -111,10 +115,20 @@ methods:{
       DistrictService.getAll().then(
           res =>{
             console.log(res.data)
-            this.district = res.data.district
+            this.district = res.data
           }
       )
+    },
+  handleSearch(e){
+      e.preventDefault();
+      this.getStreets()
+  },
+  resetButton(){
+    this.param = {
+      districtId: undefined,
+      name: undefined
     }
+  }
 }
 }
 </script>
